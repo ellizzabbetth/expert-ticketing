6255ff43933596c2c1d822bf3594feaa51e117d2
elizabeth <ellizzabbetth@gmail.com>
2022-10-10 14:14:44 -0400

    add start up msg ticket

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
-
+console.log('Starting tickets ...')
 router.get('/api/tickets', async (req: Request, res: Response) => {
   const tickets = await Ticket.find({});
 
