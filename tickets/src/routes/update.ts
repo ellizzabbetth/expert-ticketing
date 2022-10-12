d79c27ad9f24667a81b5562b4ed3fc02d508653d
elizabeth <ellizzabbetth@gmail.com>
2022-10-12 11:40:05 -0400

    ticket service test 430

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,11 +5,11 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from "@elizabethtickets/common-v2";
 import { Ticket } from "../models/ticket";
 import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
 import { natsWrapper } from "../nats-wrapper";
-
 
 const router = express.Router();
 
@@ -28,6 +28,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
